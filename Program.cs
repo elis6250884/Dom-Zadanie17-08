0f083f155a3c40868ded650fa7cfc8b2e9447d4f
Ekaterina <tebekartina@ya.ru>
2022-08-17 20:05:51 +0400

    есть код для нахождения большего и менбшего

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿ 
+    {
+ 
+        int a, b;
+ 
+        Console.WriteLine("Введите два числа");
+ 
+        a = Convert.ToInt32(Console.ReadLine());
+ 
+        b = Convert.ToInt32(Console.ReadLine());
+ 
+        if (a > b)
+ 
+        {
+ 
+            Console.WriteLine("{0} наибольшее число", a);
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+            Console.WriteLine("{0} наибольшее число ", b);
+ 
+        }
+ 
+        Console.ReadLine();
+  if (a < b)
+ 
+        {
+ 
+            Console.WriteLine("{0} наименьшее число", a);
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+            Console.WriteLine("{0} наименьшее число ", b);
+ 
+        }
+ 
+        Console.ReadLine();
+ 
+    }
